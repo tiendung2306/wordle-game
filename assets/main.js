33f975c60956d2decb4d0a9afd1a87e8d4fbf899
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-03-08 02:08:55 +0700

    finish keyboard

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,7 @@ var solution_pool = "aback abase abate abbey abbot abhor abide abled abode abort
 
 var easy_solution_pool = "about alert argue beach above alike arise began abuse alive array begin actor allow aside begun acute alone asset being admit along audio below adopt alter audit bench adult among avoid billy after anger award birth again angle aware black agent angry badly blame agree apart baker blind ahead apple bases block alarm apply basic blood album arena basis board boost buyer china cover booth cable chose craft bound calif civil crash brain carry claim cream brand catch class crime bread cause clean cross break chain clear crowd breed chair click crown brief chart clock curve bring chase close cycle broad cheap coach daily broke check coast dance brown chest could dated build chief count dealt built child court death debut entry forth group delay equal forty grown depth error forum guard doing event found guess doubt every frame guest dozen exact frank guide draft exist fraud happy drama extra fresh harry drawn faith front heart dream false fruit heavy dress fault fully hence drill fibre funny night drink field giant horse drive fifth given hotel drove fifty glass house dying fight globe human eager final going ideal early first grace image earth fixed grade index eight flash grand inner elite fleet grant input empty floor grass issue enemy fluid great irony enjoy focus green juice enter force gross joint judge metal media newly known local might noise label logic minor north large loose minus noted laser lower mixed novel later lucky model nurse laugh lunch money occur layer lying month ocean learn magic moral offer lease major motor often least maker mount order leave march mouse other legal music mouth ought level match movie paint light mayor needs paper limit meant never party peace power radio round panel press raise route phase price range royal phone pride rapid rural photo prime ratio scale piece print reach scene pilot prior ready scope pitch prize refer score place proof right sense plain proud rival serve plane prove river seven plant queen quick shall plate sixth stand shape point quiet roman share pound quite rough sharp sheet spare style times shelf speak sugar tired shell speed suite title shift spend super today shirt spent sweet topic shock split table total shoot spoke taken touch short sport taste tough shown staff taxes tower sight stage teach track since stake teeth trade sixty start texas treat sized state thank trend skill steam theft trial sleep steel their tried slide stick theme tries small still there truck smart stock these truly smile stone thick trust smith stood thing truth smoke store think twice solid storm third under solve story those undue sorry strip three union sound stuck threw unity south study throw until space stuff tight upper upset whole waste wound urban whose watch write usage woman water wrong usual train wheel wrote valid world where yield value worry which young video worse while youth virus worst white worth visit would vital voice"
 
-var isEasyMode = false;
+var isEasyMode = true;
 var all_words;
 var solution_words;
 
@@ -15,6 +15,7 @@ let curr_guess_num = 0;
 let curr_cell = 0;
 let isGameEnd = false;
 
+let all_keyboard_btn = document.querySelectorAll(".keyboard-btn");
 let gameover_panel = document.querySelector("#gameover-panel");
 let gameover_win_panel = document.querySelector("#gameover-panel #win-panel");
 let gameover_lose_panel = document.querySelector("#gameover-panel #lose-panel");
@@ -22,7 +23,7 @@ let gameover_lose_panel = document.querySelector("#gameover-panel #lose-panel");
 function toLowerCase(chr) {
     if (chr >= 'a' && chr <= 'z') return chr;
     if (chr >= 'A' && chr <= 'Z') return chr.toLowerCase();
-    return null;
+    return 0;
 }
 
 function word_input_cell_check(self, idx) {
@@ -31,7 +32,7 @@ function word_input_cell_check(self, idx) {
         self.value = "" + last_digit;
     }
 
-    if (toLowerCase(self.value[0]) < 'a' || toLowerCase(self.value[0]) > 'z')
+    if (toLowerCase(self.value[0]) < 'a'.charCodeAt() || toLowerCase(self.value[0]) > 'z'.charCodeAt())
         self.value = "";
 
     if (idx % 5 != 4 && self.value.length == 1)
@@ -125,10 +126,30 @@ function enter_btn_clicked() {
         }
         word_input_cells[i].setAttribute("disabled", "");
     }
+    for (let i = start_word_cell; i <= end_word_cell; i++) {
+        for (let j = 0; j < 26; j++) {
+            if (toLowerCase(all_keyboard_btn[j].value) == toLowerCase(word_input_cells[i].value)) {
+                if (all_keyboard_btn[j].classList.contains("totally-correct-cell")) continue;
+                if (all_keyboard_btn[j].classList.contains("wrong-position-cell")) all_keyboard_btn[j].classList.remove("wrong-position-cell");
+                if (all_keyboard_btn[j].classList.contains("totally-wrong-cell")) all_keyboard_btn[j].classList.remove("totally-wrong-cell");
+                if (check_cell_correct_letter(word_input_cells[i], i) == 2) {
+                    all_keyboard_btn[j].classList.add("totally-correct-cell");
+                }
+                else if (check_cell_correct_letter(word_input_cells[i], i) == 1) {
+                    all_keyboard_btn[j].classList.add("wrong-position-cell");
+                }
+                else {
+                    all_keyboard_btn[j].classList.add("totally-wrong-cell");
+                }
+            }
+        }
+    }
 
     if (curr_guess_num < 5) {
         word_input_cells[end_word_cell + 1].focus();
     }
+
+    curr_cell = 0;
 
     if (isGuessCorrect) {
         isGameEnd = true;
@@ -156,8 +177,8 @@ function getRndInteger(min, max) {
 }
 
 function keyboard_container_process() {
-    all_keyboard_btn = document.querySelectorAll(".keyboard-btn");
-    console.log(all_keyboard_btn);
+
+    // console.log(all_keyboard_btn);
     for (let key of all_keyboard_btn) {
         key.innerHTML = key.value;
     }
